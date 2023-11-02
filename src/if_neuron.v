422e7cd0ba86c82d88d341fad50b1de528de7f2b
mountains-high <oceandi@ASSELs-MacBook-Pro.local>
2023-11-02 14:29:18 -0700

    if_neuron model

--- src/if_neuron.v
+++ src/if_neuron.v
@@ -1,8 +1,8 @@
 
 /*
-    First-order leaky integrate-and-fire neuron model.
+    First-order integrate-and-fire neuron model.
     Input is assumed to be a current injection.
-    Membrane potential decays exponentially with rate beta.
+    
     For :math:`U[T] > U_{\\rm thr} ⇒ S[T+1] = 1`.
 
     If `reset_mechanism = "subtract"`, then :math:`U[t+1]` will have
@@ -10,21 +10,20 @@
 
     .. math::
 
-            U[t+1] = βU[t] + I_{\\rm in}[t+1] - RU_{\\rm thr}
+            U[t+1] = U[t] + I_{\\rm in}[t+1] - RU_{\\rm thr}
 
     If `reset_mechanism = "zero"`, then :math:`U[t+1]` will be set to `0`
     whenever the neuron emits a spike:
 
     .. math::
 
-            U[t+1] = βU[t] + I_{\\rm syn}[t+1] - R(βU[t] + I_{\\rm in}[t+1])
+            U[t+1] = U[t] + I_{\\rm syn}[t+1] - R(U[t] + I_{\\rm in}[t+1])
 
       :math:`I_{\\rm in}` - Input current
       :math:`U` - Membrane potential
       :math:`U_{\\rm thr}` - Membrane threshold
       :math:`R` - Reset mechanism: if active, :math:`R = 1`, otherwise \
         :math:`R = 0`
-      :math:`β` - Membrane potential decay rate
 */
 `default_nettype none
 
