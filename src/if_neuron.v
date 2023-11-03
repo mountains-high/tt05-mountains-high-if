40be8bcf33b794a554d020fef8d54d0b592bebe9
mountains-high <oceandi@eduroam-169-233-172-168.ucsc.edu>
2023-11-02 18:01:20 -0700

    kill the current

--- src/if_neuron.v
+++ src/if_neuron.v
@@ -17,14 +17,14 @@
 
     .. math::
 
-            U[t+1] = βU[t] + I_{\\rm syn}[t+1] - R(βU[t] + I_{\\rm in}[t+1])
+            U[t+1] = U[t] + I_{\\rm syn}[t+1] - R(U[t] + I_{\\rm in}[t+1])
 
       :math:`I_{\\rm in}` - Input current
       :math:`U` - Membrane potential
       :math:`U_{\\rm thr}` - Membrane threshold
       :math:`R` - Reset mechanism: if active, :math:`R = 1`, otherwise \
         :math:`R = 0`
-      :math:`β` - Membrane potential decay rate
+    
 */
 `default_nettype none
 
@@ -42,15 +42,16 @@ module if_neuron (
 always @(posedge clk) begin
         if (!rst_n) begin
             state <= 0;
-            threshold <= 230;
+            threshold <= 200;
         end else begin
             state <= next_state;
         end
     end
 
-    // next_state logic and spiking logic
+    // next_state logic and spiking logic for IF
     assign spike = (state >= threshold);
     assign next_state = (spike ? 0 : (state + current)) - (spike ? threshold : 0);
+    
 
 
 endmodule
--- src/test.py
+++ src/test.py
@@ -5,22 +5,29 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 @cocotb.test()
 async def test_my_design(dut):
 
-    CONSTANT_CURRENT = 85 # For example, injecting some current
-    
+    CONSTANT_CURRENT = 50  # For example, injecting some current
+    OFF_CURRENT = 0
+
     dut._log.info("start simulation")
 
-    # initialize clock
+    # Initialize clock
     clock = Clock(dut.clk, 1, units="ns")
     cocotb.start_soon(clock.start())
 
-    dut.rst_n.value = 0 # low to reset
+    # Reset the circuit.
+    dut.rst_n.value = 0  # low to reset
     await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1 # take out of reset
+    dut.rst_n.value = 1  # take out of reset
 
-    dut.ui_in.value = CONSTANT_CURRENT
-    dut.ena.value = 1 # enable design
+    # Initialize input values
+    dut.ena.value = 1  # enable design
 
-    for _ in range(100):  # run for 100 clock cycles
+    for i in range(100):  # run for 100 clock cycles
+        if 50 <= i <= 80:
+            dut.ui_in.value = OFF_CURRENT
+        else:
+            dut.ui_in.value = CONSTANT_CURRENT
+
         await RisingEdge(dut.clk)
-    
+
     dut._log.info("Finished test!")
